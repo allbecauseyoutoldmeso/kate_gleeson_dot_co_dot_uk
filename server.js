5b0d652d6472bb105a963ea27422a3386b3de5d3
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-06-20 13:16:02 +0100

    pass info to index ejs file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response){
-  response.render('index');
+  response.render('index',{name:'Kate'})
 });
 
 server.listen(3000, function(){
